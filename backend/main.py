eee48a55f1b7a1b7b01fea9c40830aeefa3c6d7b
marketcalls <rajandran@marketcalls.in>
2024-05-19 20:51:21 +0530

    modified:   backend/__pycache__/main.cpython-311.pyc 	modified:   backend/main.py 	modified:   backend/requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,28 @@
 from fastapi import FastAPI
 from databases import Database
-import redis.asyncio as aioredis
+import redis.asyncio as redis
 
 DATABASE_URL = "postgresql://myuser:mypassword@postgres/mydb"
 REDIS_URL = "redis://redis:6379"
 
 database = Database(DATABASE_URL)
-redis = aioredis.from_url(REDIS_URL)
+redis_client = redis.from_url(REDIS_URL)
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
-    await redis.ping()
+    await redis_client.ping()
 
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-    await redis.close()
+    await redis_client.close()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Development Environement"}
+    return {"Hello": "Development Environment"}
 
 @app.get("/postgres")
 async def postgres():
@@ -32,6 +32,6 @@ async def postgres():
 
 @app.get("/redis")
 async def redis():
-    await redis.set("key", "value")
-    value = await redis.get("key")
+    await redis_client.set("key", "value")
+    value = await redis_client.get("key")
     return {"Redis": value.decode("utf-8")}
